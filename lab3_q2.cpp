56f2d6ddb429851517f7f85cd8fc306d37d2db39
rajesh-06 <42206081+rajesh-06@users.noreply.github.com>
2018-08-20 15:52:20 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,18 @@
+// library
+#include<iostream>
+using namespace std;
+//write the name function
+int main()
+{//declare the variables
+int a = 10;
+char b = 'b';
+float c = 4.78;
+double d = 2.57674;
+bool e = 1;
+// print the variables having different data types
+cout << " a sample integer is " << a << " and the size is " << sizeof(a) << "\n";
+cout << " a sample character is " << b << " and the size is " << sizeof(b) << "\n";
+cout << " a sample float is " << c << " and the size is " << sizeof(c) << "\n";
+cout << " a sample double is " << d << " and the size is " << sizeof(d) << "\n";
+cout << " a sample boolean is " << e << " and the size is " << sizeof(e) << "\n";
+return 0;}
